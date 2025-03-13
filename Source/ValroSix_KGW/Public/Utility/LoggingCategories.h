89a429855fe5edc2c02dbe3f31a9383d86ea1fa5
gwo1004 <mynameiskgw@gmail.com>
2025-03-13 15:19:07 +0900

    [Add] 로그 필터를 위한 카테고리 일부 추가

---
+++ Source/ValroSix_KGW/Public/Utility/LoggingCategories.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+// Custom Common Log
+DECLARE_LOG_CATEGORY_EXTERN(LogGameCore, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogPlayer, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogAbility, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogWeapon, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogMesh, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogUI, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogReplication, Log, All);
