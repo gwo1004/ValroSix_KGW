80c5386be7d6f626cf73cbeead85df7abff2696a
gwo1004 <mynameiskgw@gmail.com>
2025-03-14 15:20:18 +0900

    [Update] IMC 관련 로직 구현

--- Source/ValroSix_KGW/Private/Core/Player/BasePlayerController.cpp
+++ Source/ValroSix_KGW/Private/Core/Player/BasePlayerController.cpp
@@ -2,8 +2,35 @@
 
 
 #include "Core/Player/BasePlayerController.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "Data/DataAssets/PlayerInputKeyData.h"
 
 ABasePlayerController::ABasePlayerController()
+{
+	CurrentMode = EControlMode::NONE;
+
+	ControlModeMap.Add(EControlMode::Character, FControlModeMapping());
+	ControlModeMap.Add(EControlMode::SkillPawn, FControlModeMapping());
+}
+
+void ABasePlayerController::SwitchControlMode(EControlMode Mode)
+{
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* SubSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			if (FControlModeMapping* ModeData = ControlModeMap.Find(Mode))
+			{
+				SubSystem->ClearAllMappings();
+				SubSystem->AddMappingContext(ModeData->IMC, 0);
+				CurrentMode = Mode;
+			}
+		}
+	}
+}
+
+void ABasePlayerController::UpdateCurrentIMC(UPlayerInputKeyData* CurrentDataAsset)
 {
 }
 
--- Source/ValroSix_KGW/Public/Core/Player/BasePlayerController.h
+++ Source/ValroSix_KGW/Public/Core/Player/BasePlayerController.h
@@ -9,8 +9,28 @@
 /**
  * 
  */
-
 class UInputMappingContext;
+class UPlayerInputKeyData;
+
+UENUM(BlueprintType)
+enum class EControlMode : uint8
+{
+	NONE	  UMETA(DisplayName = "None"),
+	Character UMETA(DisplayName = "Character"),
+	SkillPawn UMETA(DisplayName = "SkillPawn")
+};
+
+USTRUCT(BlueprintType)
+struct FControlModeMapping
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	UInputMappingContext* IMC;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	UPlayerInputKeyData* DataAsset;
+};
 
 UCLASS()
 class VALROSIX_KGW_API ABasePlayerController : public APlayerController
@@ -19,10 +39,21 @@ class VALROSIX_KGW_API ABasePlayerController : public APlayerController
 	
 public:
 	ABasePlayerController();
+	
+	UFUNCTION()
+	void SwitchControlMode(EControlMode Mode);
+
+	UFUNCTION()
+	void UpdateCurrentIMC(UPlayerInputKeyData* CurrentDataAsset);
 
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
 	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Enhanced Input")
+	TMap<EControlMode, FControlModeMapping> ControlModeMap;
 	
+private:
+	EControlMode CurrentMode;
 };
