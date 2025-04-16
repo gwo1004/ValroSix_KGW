de9a5c0bd5c4d48ce7916fc244708901263b9f40
gwo1004 <mynameiskgw@gmail.com>
2025-04-17 02:00:13 +0900

    [Update] UI 상점 관련 WBP Item 디자인

---
+++ Source/ValroSix_KGW/Private/UI/Views/Shop/WeaponItemWidget.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Views/Shop/WeaponItemWidget.h"
+
+void UWeaponItemWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void UWeaponItemWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UWeaponItemWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
+
+void UWeaponItemWidget::HoverItem()
+{
+}
+
+void UWeaponItemWidget::EquipItem()
+{
+}
