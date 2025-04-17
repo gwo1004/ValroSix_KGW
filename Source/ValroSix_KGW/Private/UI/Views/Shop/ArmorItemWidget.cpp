61943a3b15351f4acfd757542920a2b813c4406c
gwo1004 <mynameiskgw@gmail.com>
2025-04-17 16:32:45 +0900

    [Add] 상점 방어구 관련 클래스 추가

---
+++ Source/ValroSix_KGW/Private/UI/Views/Shop/ArmorItemWidget.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Views/Shop/ArmorItemWidget.h"
+
+void UArmorItemWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+}
+
+void UArmorItemWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UArmorItemWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+}
+
+void UArmorItemWidget::HoverItem()
+{
+}
+
+void UArmorItemWidget::EquipItem()
+{
+}
---
+++ Source/ValroSix_KGW/Public/UI/Views/Shop/ArmorItemWidget.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Views/Shop/BaseItemWidget.h"
+#include "ArmorItemWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VALROSIX_KGW_API UArmorItemWidget : public UBaseItemWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void NativeOnInitialized() override;
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+	virtual void HoverItem() override;
+
+	virtual void EquipItem() override;
+	
+	
+};
